66d91ef280bbb32cfb4d9b9a8d03306488f4dfc3
donjoe <giumontanaro@gmail.com>
2018-12-15 14:47:09 +0100

    Updates

---
+++ precache-manifest.5b23e401459e7293047649eb78f3cecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "4364227eaf586edc1e6c",
+    "url": "/markdown-previewer/static/js/main.4364227e.chunk.js"
+  },
+  {
+    "revision": "83dd7ba6babbb7b250d2",
+    "url": "/markdown-previewer/static/js/1.83dd7ba6.chunk.js"
+  },
+  {
+    "revision": "4364227eaf586edc1e6c",
+    "url": "/markdown-previewer/static/css/main.9505aa93.chunk.css"
+  },
+  {
+    "revision": "66a82e1da11b393dc93eb94f1e2cc223",
+    "url": "/markdown-previewer/index.html"
+  }
+];
