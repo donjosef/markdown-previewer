04ac6dfb0b56d59c33b573f1962c58ea41f07542
donjoe <giumontanaro@gmail.com>
2018-12-15 15:23:30 +0100

    Set initial state for markdown to pass fcc test

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Previewer from './components/Previewer/Previewer';
 import './App.css';
 
 const marked = require('marked');
-
+const initialMarkdown = '# Im an h1\n\n## Im an h2\n\n[link](www.google.com)\n\nInline code `<div></div>`\n\n```\nMulti line code:\n\n function foo() {}\n```\n\n* item 1\n* item 2\n\n> Block Quotes!\n\nEmbedded images:\n![React Logo w/ Text](https://goo.gl/Umyytc)\n\n**Bold Text**';
 
 class App extends Component {
 
   state = {
-    markdown: '# Im an h1'
+    markdown: initialMarkdown
   }
 
   changeTextHandler = (e) => {
